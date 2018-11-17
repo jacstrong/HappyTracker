964e2d33cb92b12016576798036c4e05a8a68023
scowley <skyler.cowley@gmail.com>
2018-11-17 04:15:33 -0700

    asdf2

--- src/lambda/chunk.js
+++ src/lambda/chunk.js
@@ -7,9 +7,9 @@ var mongoose = require('mongoose');
 let conn = null;
 
 const uri = `mongodb://${process.env.mdb_username}:${process.env.mdb_password}@${process.env.mongourl}:${process.env.mdb_port}/${process.env.mdb_databaseName}`;
-console.log(uri);
 exports.handler = function(event, context, callback) {
 
+  console.log(uri);
   context.callbackWaitsForEmptyEventLoop = false;
 
   run().
